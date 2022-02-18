d5431e290160ca1806c3b5a9e5cbb605d8987ceb
RasulovBoburjon <boburgithub@gmail.com>
2022-02-18 16:36:43 +0300

    javascriptprotica

---
+++ menu bars top/main.js
@@ -0,0 +1,9 @@
+
+
+function myBars() { 
+    document.getElementById('navbar').style.height = "40%";
+ }
+
+ function closeNav(){ 
+     document.getElementById("navbar").style.height = '0%';   
+  }
---
+++ menu full bars/main.js
@@ -0,0 +1,9 @@
+
+
+function myMenu(){
+    document.getElementById('navbar').style.width = "100%";
+}
+
+function myClose(){
+    document.getElementById('navbar').style.width = "0";
+}
